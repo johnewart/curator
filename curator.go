b44788afc8bd3e54964a39a8cdf68212699cdc54
John Ewart <john@unixninjas.org>
2014-11-03 18:49:12 -0800

    gofmt

--- curator.go
+++ curator.go
@@ -10,51 +10,51 @@ import "strings"
 import "io"
 import "path"
 import "path/filepath"
+import "gofmt"
 
-type OutputStreams struct { 
-  Stdout OutputStream
-  Stderr OutputStream
+type OutputStreams struct {
+	Stdout OutputStream
+	Stderr OutputStream
 }
 
 type OutputStream struct {
-  LogFile LogFile `yaml:"log_file"`
+	LogFile LogFile `yaml:"log_file"`
 }
 
-type LogFile struct { 
-  Path string
-  Maxbytes int
-  Backups int
+type LogFile struct {
+	Path     string
+	Maxbytes int
+	Backups  int
 }
-
 
 type Program struct {
-   Command string
-   ProcessName string  `yaml:"process_name"`
-   Numprocs int
-   NumprocsStart int `yaml:"numprocs_start"`
-   Priority int
-   Autostart bool
-   Autorestart bool
-   StartSecs int `yaml:"start_secs"`
-   StartRetries int `yaml:"start_retries"`
-   ExitCodes []int `yaml:"exit_codes"`
-   StopSignal string `yaml:"stop_signal"`
-   StopWaitSecs int `yaml:"stop_wait_secs"`
-   User string
-   RedirectStderr bool `yaml:"redirect_stderr"`
-   Environment map[string]string
-   Directory string
-   ServerURL string `yaml:"server_url"`
-   OutputStreams OutputStreams  `yaml:"output_streams"`
+	Command        string
+	ProcessName    string `yaml:"process_name"`
+	Numprocs       int
+	NumprocsStart  int `yaml:"numprocs_start"`
+	Priority       int
+	Autostart      bool
+	Autorestart    bool
+	StartSecs      int    `yaml:"start_secs"`
+	StartRetries   int    `yaml:"start_retries"`
+	ExitCodes      []int  `yaml:"exit_codes"`
+	StopSignal     string `yaml:"stop_signal"`
+	StopWaitSecs   int    `yaml:"stop_wait_secs"`
+	User           string
+	RedirectStderr bool `yaml:"redirect_stderr"`
+	Environment    map[string]string
+	Directory      string
+	ServerURL      string        `yaml:"server_url"`
+	OutputStreams  OutputStreams `yaml:"output_streams"`
 }
 
 type State int
 
 const (
-  STOPPED State = iota
-  STARTING
-  RUNNING
-  STOPPING
+	STOPPED State = iota
+	STARTING
+	RUNNING
+	STOPPING
 )
 
 func (s State) String() string {
@@ -63,137 +63,136 @@ func (s State) String() string {
 		return "STOPPED"
 	case STARTING:
 		return "STARTING"
-  case RUNNING: 
-    return "RUNNING"
-  case STOPPING:
-    return "STOPPING"
+	case RUNNING:
+		return "RUNNING"
+	case STOPPING:
+		return "STOPPING"
 	}
 	return ""
 }
 
-
-type Process struct { 
-  Program Program
-  Command *exec.Cmd
-  State State
+type Process struct {
+	Program Program
+	Command *exec.Cmd
+	State   State
 }
 
-var processTable map[string]Process 
+var processTable map[string]Process
 
 // TODO: check output file size and rotate.
 func flushStream(stdoutPipe io.ReadCloser, writer *bufio.Writer) {
-    buffer := make([]byte, 100, 1000)
-    for ;; {
-        n, err := stdoutPipe.Read(buffer)
-        if err == io.EOF {
-            stdoutPipe.Close()
-            break
-        }
-        buffer = buffer[0:n]
-        writer.Write(buffer)
-        writer.Flush()
-    }
+	buffer := make([]byte, 100, 1000)
+	for {
+		n, err := stdoutPipe.Read(buffer)
+		if err == io.EOF {
+			stdoutPipe.Close()
+			break
+		}
+		buffer = buffer[0:n]
+		writer.Write(buffer)
+		writer.Flush()
+	}
 }
 
 func spawnProcess(p Program) {
-      fmt.Printf("Process: %s\n", p.ProcessName)
-      stdoutPath := p.OutputStreams.Stdout.LogFile.Path
-      stderrPath := p.OutputStreams.Stderr.LogFile.Path
+	fmt.Printf("Process: %s\n", p.ProcessName)
+	stdoutPath := p.OutputStreams.Stdout.LogFile.Path
+	stderrPath := p.OutputStreams.Stderr.LogFile.Path
 
-      fmt.Printf("Writing STDOUT to %s\n", stdoutPath)
-      fmt.Printf("Writing STDERR to %s\n", stderrPath)
-      outf, err := os.Create(stdoutPath)
-      if err != nil {
-        panic(err)
-      }
+	fmt.Printf("Writing STDOUT to %s\n", stdoutPath)
+	fmt.Printf("Writing STDERR to %s\n", stderrPath)
+	outf, err := os.Create(stdoutPath)
+	if err != nil {
+		panic(err)
+	}
 
-      errf, err := os.Create(stderrPath) 
-      if err != nil {
-        panic(err)
-      }
+	errf, err := os.Create(stderrPath)
+	if err != nil {
+		panic(err)
+	}
 
-      outw := bufio.NewWriter(outf)
-      errw := bufio.NewWriter(errf)
-      
-      fmt.Printf("Going to run %s\n", p.Command)
-      cmdParts := strings.Split(p.Command, " ")
-      fmt.Println(cmdParts)
+	outw := bufio.NewWriter(outf)
+	errw := bufio.NewWriter(errf)
 
-       
-      cmd := exec.Command(cmdParts[0], cmdParts[1:]...)
-      stdoutPipe, _ := cmd.StdoutPipe()
-      stderrPipe, _ := cmd.StderrPipe()
+	fmt.Printf("Going to run %s\n", p.Command)
+	cmdParts := strings.Split(p.Command, " ")
+	fmt.Println(cmdParts)
 
-      e := cmd.Start()
-      if e != nil {
-        panic(e)
-      }
+	cmd := exec.Command(cmdParts[0], cmdParts[1:]...)
+	stdoutPipe, _ := cmd.StdoutPipe()
+	stderrPipe, _ := cmd.StderrPipe()
 
-      go flushStream(stdoutPipe, outw)
-      go flushStream(stderrPipe, errw)
+	e := cmd.Start()
+	if e != nil {
+		panic(e)
+	}
 
-      defer cmd.Wait()
-      proc := Process{Program: p, Command: cmd, State: RUNNING}
-      fmt.Printf("Adding %s to process table\n", p.ProcessName)
-      processTable[p.ProcessName] = proc
+	go flushStream(stdoutPipe, outw)
+	go flushStream(stderrPipe, errw)
+
+	defer cmd.Wait()
+	proc := Process{Program: p, Command: cmd, State: RUNNING}
+	fmt.Printf("Adding %s to process table\n", p.ProcessName)
+	processTable[p.ProcessName] = proc
 }
 
 func handleInput(channel chan string, kbd *bufio.Reader) {
-    for {
-      fmt.Print("CMD: ")
-      text, _ := kbd.ReadString('\n')
-      text = strings.TrimSpace(text)
-      channel <- text
-    }
+	for {
+		fmt.Print("CMD: ")
+		text, _ := kbd.ReadString('\n')
+		text = strings.TrimSpace(text)
+		channel <- text
+	}
 }
 
 func handleConfig(configFile string) {
-    var p Program
-    var fileData, err = ioutil.ReadFile(configFile)
+	var p Program
+	var fileData, err = ioutil.ReadFile(configFile)
 
-    if (err != nil) {
-      panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 
-    err = yaml.Unmarshal(fileData, &p)
-    if err != nil {
-      panic(err)
-    }
+	err = yaml.Unmarshal(fileData, &p)
+	if err != nil {
+		panic(err)
+	}
 
-    go spawnProcess(p)
+	go spawnProcess(p)
 }
 
 func loadConfigs(srcDir string) {
-  globStr := path.Join(srcDir, "*.yml")
-  fmt.Printf("Loading files matching %s\n", globStr)
-  configFiles, _ := filepath.Glob(globStr)
-  for _,configFile := range configFiles { 
-    fmt.Printf("Loading %s...\n", configFile)
-    handleConfig(configFile)
-  }
+	globStr := path.Join(srcDir, "*.yml")
+	fmt.Printf("Loading files matching %s\n", globStr)
+	configFiles, _ := filepath.Glob(globStr)
+	for _, configFile := range configFiles {
+		fmt.Printf("Loading %s...\n", configFile)
+		handleConfig(configFile)
+	}
 }
 
 func main() {
-    processTable = make(map[string]Process)
-    queue := make(chan string, 1)
-    cwd, _ := os.Getwd()
-    loadConfigs(cwd)
+	processTable = make(map[string]Process)
+	queue := make(chan string, 1)
+	cwd, _ := os.Getwd()
+	loadConfigs(cwd)
 
-    reader := bufio.NewReader(os.Stdin)
-    go handleInput(queue, reader)
+	reader := bufio.NewReader(os.Stdin)
+	go handleInput(queue, reader)
 
-    for { 
-      select {
-        case val := <-queue:
-          switch val {
-            case "STATUS": {
-              fmt.Println("Checking status...") 
-              for k, p := range processTable  {
-                fmt.Printf("%s: %s\n", k, p.State)
-              }
-            }
-          }
-      }
-    }
+	for {
+		select {
+		case val := <-queue:
+			switch val {
+			case "STATUS":
+				{
+					fmt.Println("Checking status...")
+					for k, p := range processTable {
+						fmt.Printf("%s: %s\n", k, p.State)
+					}
+				}
+			}
+		}
+	}
 
 }
